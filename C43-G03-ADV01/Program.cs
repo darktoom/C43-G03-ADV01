3f2b0fc9b94ef080d37c98ffd9e6fb5cde3d2411
darktoom <darktoom789@gmail.com>
2025-02-06 16:38:39 +0200

    Add project files.

---
+++ C43-G03-ADV01/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace C43_G03_ADV01
+{
+    internal class Program
+    {
+        static void OptimizedBubble(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                     
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        swapped = true; 
+                    }
+                }
+
+              
+                if (!swapped)
+                    break;
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            Console.WriteLine("Original array:");
+            PrintArray(arr);
+
+            OptimizedBubble(arr);
+
+            Console.WriteLine("Sorted array:");
+            PrintArray(arr);
+        }
+    }
+}
